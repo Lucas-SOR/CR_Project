ed65dc48062e06d86c24ce8c9705295b4ccde162
ArianeDlns <ariane.dalens@student.ecp.fr>
2022-02-22 15:52:53 +0100

    Update Game Map

--- streamlit.py
+++ streamlit.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from utils.map import *
 
 # ----------------------------------------------------------------------------------------------------------------------
 # To run locally run: 'streamlit run streamlit.py'
@@ -11,3 +12,9 @@ def load_data(path):
     return data
 
 st.sidebar.image("https://media.giphy.com/media/mf8UbIDew7e8g/giphy.gif")
+
+
+game_data = {'Ger': ['Germany','DEU', 'True'], 'Fr': ['France','FRA','False']}
+game_data =  pd.DataFrame.from_dict(game_data, orient='index',columns = ['countries','ISO','played'])
+
+st.plotly_chart(show_map_game(game_data), use_container_width=False)
